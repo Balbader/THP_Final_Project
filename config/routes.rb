0e10f17696f9c8d61a7dc2c179b8cda02e3d2f3f
Balbader <15971200+Balbader@users.noreply.github.com>
2021-03-18 18:57:44 +0100

    fix of routes for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,18 @@ Rails.application.routes.draw do
   get 'static_pages/secret'
 
   get 'static_pages/stripe_button'
-  get 'about_us', to: 'static_pages#about_us'
-  get 'rules', to: 'static_pages#rules'
-  get 'faq', to: 'static_pages#faq'
-  get 'community', to: 'static_pages#community'
-  get 'terms', to: 'static_pages#terms'
-  get 'privacy', to: 'static_pages#privacy'
+
+  # Static Pages routes
+  scope '/', controller: :static_pages do
+    get :terms, :community, :privacy, :rules, :faq, :about_us, :stripe_button, :secret
+  end
 
   resources :categories, only: [:show, :index]
   resources :trivia
   resources :answers
   post "/trivia/:id", to: "trivia#trivia_answer", as: "trivia_answer"
   devise_for :users
-  
+
   # Stripe scope
   scope '/orders' do
     post 'create', to: 'orders#create', as: 'orders_create'
