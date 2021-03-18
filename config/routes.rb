4d2074bd04ddea6c3930c68d7c00746f92f2bf9e
Charles <charlescampet03@gmail.com>
2021-03-18 09:53:36 +0100

    add new routes and stripe script added to app/layout

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Rails.application.routes.draw do
   resources :categories, only: [:show, :index]
   resources :trivia
   devise_for :users
-  resources :orders, only: [:new, :create]
+  
+
+  # Routes for stripe checkout
+
+  scope '/orders' do
+    post 'create', to: 'orders#create', as: 'orders_create'
+    get 'cancel', to: 'orders#cancel', as: 'orders_cancel'
+    get 'success', to: 'orders#success', as: 'orders_success'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
