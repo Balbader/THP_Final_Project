d2e2281bcf3cbd59272e17978e621bd550d26444
Youssef <annabiyoussef1986@gmail.com>
2021-03-23 16:50:08 +0100

    fixing editing user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-    def after_sign_in_path_for(resource)
-        trivia_path
-       end
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def after_sign_in_path_for(resource)
+    trivia_path
+   end
+   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation) }
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name,:age, :current_password) }
+  end
 end
+  
