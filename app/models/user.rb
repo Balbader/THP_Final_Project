61399dbe00609862300a8994b50f826d0ba4c4cf
Youssef <annabiyoussef1986@gmail.com>
2021-03-24 12:33:33 +0100

    adding one attached avatar to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          has_many :trivia_users
          has_many :trivia, through: :trivia_users
+         has_one_attached :avatar
 
          after_create :welcome_send
 
